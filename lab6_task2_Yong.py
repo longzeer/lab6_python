408f10006fd1505f2c4f8db76f93526778f32ed2
longzeer <shao0015@algonquinlive.com>
2019-10-15 01:01:40 -0400

    Task2 modified 1st

--- lab6_task2_Yong.py
+++ lab6_task2_Yong.py
@@ -25,6 +25,26 @@ pm = [[0,0,0,1,1,1,1,1,0,0,0],
 [0,0,0,1,1,1,1,1,0,0,0]
 ]
 
+
+#function
+
+def displayObject(f1,x,y):
+    lcd.clear()
+    lcd.show()
+    y1=y
+    for i in f1:
+        y1+=1
+        x1=x
+        for j in i:                
+          x1+=1
+          lcd.set_pixel(x1,y1,j)
+          lcd.show()
+
+#test
+displayObject(pm,15,20)
+
+
+# The new object
 yh = [
 [1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
 [0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0],
@@ -41,16 +61,5 @@ yh = [
 
 ]
 
-def displayObject(f1):
-    lcd.clear()
-    lcd.show()
-    x=int(input("please enter the x(<64) of the coordinate "))
-    y=int(input("please enter the Y(<32) of the coordinate "))
-    y1=y
-    for i in f1:
-        y1+=1
-        x1=x
-        for j in i:                
-          x1+=1
-          lcd.set_pixel(x1,y1,j)
-          lcd.show()
+#The program
+displayObject(yh,int(input("please enter the x(<64) of the coordinate ")),int(input("please enter the y(<32) of the coordinate ")))
